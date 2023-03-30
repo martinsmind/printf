d2cfa8b7074988521e9913fc26e4ed3eca6f6af8
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-03-30 08:02:03 +0000

    editted and changed for fix

--- string-fields.c
+++ string-fields.c
@@ -12,21 +12,33 @@
 
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int d = 0;
+int d = 0;
 
-	if (*p != '.')
-		return (p);
-	p++;
-	if (*p == '*')
-	{
-		d = va_arg(ap, int);
-		p++;
-	}
-	else
-	{
-		while (_isdigit(*p))
-			d = d * 10 + (*p++ - '0');
-	}
-	params->precision = d;
-	return (p);
+if (*p != '.')
+return (p);
+p++;
+if (*p == '*')
+{
+d = va_arg(ap, int);
+if (d < 0)
+params->precision = -1;  /* negative precision means no precision */
+else
+params->precision = d;
+p++;
+}
+else
+{
+if (*p == '0')
+{
+params->precision = 0;
+p++;
+}
+else
+{
+while (_isdigit(*p))
+d = d * 10 + (*p++ - '0');
+params->precision = d;
+}
+}
+return (p);
 }
